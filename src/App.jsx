ffccd79463950dd1388633fb6e34e3b4c25aa3a7
Edison Matos <edison.matos@live.com>
2025-07-16 15:01:45 -0300

    feat: Comenta parte de feedbacks

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
     <div className="flex flex-col max-w-[1400px] ">
       <Clientes />
       <Servicos />
-      <Feedbacks />
+      {/* <Feedbacks /> */}
       <QuadroSuporte />
       <QuadroDev />
       <QuadroWebmaster />
