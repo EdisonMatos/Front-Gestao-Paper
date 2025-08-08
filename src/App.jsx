7be851ece9dca74448cf0ca501cc3ea3467babb0
Edison Matos <edison.matos@live.com>
2025-08-07 22:20:51 -0300

    feat: Adiciona logo da Paper Club

--- src/App.jsx
+++ src/App.jsx
@@ -30,11 +30,19 @@ function App() {
   return (
     <div className="min-h-screen bg-background font-mainFont">
       <ToastContainer />
-      <img className="pl-6 w-[350px]" src={paperClubLogo}></img>
-      {/* <h1 className="ml-6 text-3xl font-bold text-text/70">
-        Sistema de Gerenciamento
-      </h1>
-      <p className="ml-6 text-text/50">Versão 0.8 (Em desenvolvimento)</p> */}
+      <div className="flex items-center">
+        <img className="pl-6 w-[200px]" src={paperClubLogo}></img>
+        <div>
+          <h1 className="ml-6 text-3xl font-bold text-text">Paper Club</h1>
+          <h1 className="ml-6 text-md text-text">
+            Sistema de Controle Interno
+          </h1>
+          <p className="ml-6 text-sm text-text/50">Paper Street Softwares ®</p>
+          <p className="ml-6 text-sm text-text/50">
+            2025 - Versão 0.9 (Em desenvolvimento)
+          </p>
+        </div>
+      </div>
       <div className=" flex flex-col max-w-[1700px] overflow-x-auto">
         <Agrupamento>
           <Titulos>💼 Clientes e Serviços</Titulos>
