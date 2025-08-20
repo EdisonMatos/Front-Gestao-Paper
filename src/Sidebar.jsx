522fc8b8dc6ead64e82063c9904b5b841ac7586f
Edison Matos <edison.matos@live.com>
2025-08-20 08:06:03 -0300

    feat: Limpa componentes

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -251,11 +251,7 @@ export default function Sidebar() {
                         Cadastros
                       </span>
                     </div>
-                    <div>
-                      {/* <span class="inline-flex items-center justify-center w-3 h-3 p-3 ms-3 text-sm font-medium text-blue-800 bg-blue-100 rounded-full dark:bg-inputBg dark:text-white">
-                        0
-                      </span> */}
-                    </div>
+                    <div></div>
                   </button>
                 </li>
               </ul>
