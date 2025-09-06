8063ed4ab72e21a0a0b254dd2638ce5980d929f8
Edison Matos <edison.matos@live.com>
2025-09-06 11:10:26 -0300

    feat: Atualiza colunas do setor financeiro

--- src/modulos/quadros/QuadroFinanceiro.jsx
+++ src/modulos/quadros/QuadroFinanceiro.jsx
@@ -3,11 +3,25 @@ import QuadroKanban from "./QuadroKanban";
 export default function QuadroFinanceiro() {
   const colunas = {
     backlog: { nome: "Backlog", tipoCard: "normal" },
+    suspenso: { nome: "Suspensos", tipoCard: "compacto" },
     // emProgresso: { nome: "Em progresso", tipoCard: "normal" },
     enviadoComercial: { nome: "Enviado pro Comercial", tipoCard: "compacto" },
-    aguardandoCliente: { nome: "Aguardando Cliente", tipoCard: "compacto" },
+
+    aguardandoClienteAss: {
+      nome: "Aguardando Assinatura",
+      tipoCard: "compacto",
+    },
+    emAtrasoAss: { nome: "Em Atraso Assinatura", tipoCard: "compacto" },
+    aguardandoClienteAqui: {
+      nome: "Aguardando Aquisição",
+      tipoCard: "compacto",
+    },
+    emAtrasoAqui: { nome: "Em Atraso Aquisição", tipoCard: "compacto" },
     pagarComissao: { nome: "Fila de Pgto Comissão", tipoCard: "compacto" },
-    suspenso: { nome: "Suspensos", tipoCard: "compacto" },
+    pgtoPendente: {
+      nome: "Pagamento Pendente",
+      tipoCard: "compacto",
+    },
   };
 
   return (
